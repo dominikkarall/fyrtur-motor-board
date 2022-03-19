a1305d854edec4b0687ddc95d0648c66e5278221
dominikkarall <dominik.karall@gmail.com>
2022-03-19 13:14:42 +0100

    change max motor current

--- Core/Inc/motor.h
+++ Core/Inc/motor.h
@@ -29,7 +29,7 @@
 
 // The maximum amount of current that motor can draw while moving. Used for stall detection at the top position.
 // Note that this is upper limit is not enforced when starting the movement in order to break the static friction!
-#define DEFAULT_MAX_MOTOR_CURRENT 1024 // mA
+#define DEFAULT_MAX_MOTOR_CURRENT 2000 // mA
 
 // We want at least this many sensor ticks to assume the motor is moving (and free of static friction). In reality
 // this should be lower number, but it can take few ticks before the measured current starts to drop even though static friction
